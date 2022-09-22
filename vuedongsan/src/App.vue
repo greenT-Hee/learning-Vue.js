a70c90fe48cb1f3193333a425ee9f32890b28c86
greenT-Hee <heehee970322@gmail.com>
2022-09-22 16:25:13 +0900

    Add:데이터바인딩과 반복문

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,18 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" >
+  <h1>쀼동산</h1>
+  <div class="menu">
+    <a href="#" v-for="(a,i) in 메뉴" :key="i">{{ a }}</a>
+  </div>
+  <!-- <div v-for="(a, i) in products" :key="i">
+    <h4>{{ a }}</h4>
+    <p>90 만원</p>
+  </div> -->
   <div>
-    <h4>잠실 오피스텔</h4>
-    <p>XX 만원</p>
+    <h4>{{products[0]}}</h4>
+    <p>50 만원</p>
   </div>
   <div>
-    <h4>잠실 빌라</h4>
-    <p>XX 만원</p>
+    <h4>{{products[1]}}</h4>
+    <p>50 만원</p>
+  </div>
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>50 만원</p>
   </div>
 </template>
 
 <script>
 
 export default {
+  data() {
+    return {
+      메뉴 : ['HOME', 'SHOP', "ABOUT"],
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸']
+    }
+  },
   name: 'App',
   components: {
   }
@@ -26,6 +43,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: rgb(49, 175, 127);;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: #fff;
+  padding: 10px;
+  text-decoration: none;
 }
 </style>
